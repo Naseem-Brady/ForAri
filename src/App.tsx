5080d53629db45d3d094e69dbcba7cef13f6a5ac
Naseem-Brady <89871978+Naseem-Brady@users.noreply.github.com>
2024-02-13 13:37:22 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,21 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "Are you positive?",
+      "What if I gave you head?",
+      "BRO PLEASE",
+      "I'll give you reese pieces? :O",
+      "What about a nice portion of twix bars? :)",
+      "PLEASE SWEETNESS",
+      "damn that's actually crazy :(",
+      "Im gonna become unalive :*(",
+      "Yuh... just died. Your fault... (X o X)",
+      "still no? :(",
+      "youre brutal asf >:(",
+      ":(((( hatin' ass",
+      "BABY PLEASE",
+      "that hurted :((((",
+      "oh... :'(",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -37,14 +37,14 @@ export default function Page() {
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExODl6cW45ajYwNWxlaTVoYmducjV3bGM4aDh6OW5mM2N5MXltaDlneSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/i1ifSvtaxp4YSWX4o4/giphy.gif" />
+          <div className="my-4 text-4xl font-bold">AHHHHHH I LOVE YOU !!! :DDD ;)) </div>
         </>
       ) : (
         <>
           <img
             className="h-[200px]"
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWw4bnFucTZwNzZ6NGw4cnVhN2ZwdTV0bXg0bnRycWtrbHU0b3A2eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DiplElzCac5f9ERHen/giphy.gif"
           />
           <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
           <div className="flex items-center">
